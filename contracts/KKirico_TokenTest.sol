56ea7093da78b612086892def743846b3ec40599
kkirico-sanga <yasingco@naver.com>
2021-12-01 01:59:20 +0900

    Update Transfer Test in test js

--- contracts/KKirico_TokenTest.sol
+++ contracts/KKirico_TokenTest.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract KKirico_Token is Initializable, ERC20Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -14,7 +15,7 @@ contract KKirico_Token is Initializable, ERC20Upgradeable, OwnableUpgradeable, U
         __ERC20_init("tx0x1119", "Txxx");
         __Ownable_init();
         __UUPSUpgradeable_init();
-        _mint(msg.sender,500*10**decimals());
+        _mint(msg.sender,100000000000*10**decimals());
         
     }
 
@@ -23,6 +24,15 @@ contract KKirico_Token is Initializable, ERC20Upgradeable, OwnableUpgradeable, U
         onlyOwner
         override
     {}
+    function withdraw(uint withdraw_amount) public{
+        require(withdraw_amount <= 100000000000000000); 
+     // function transfer(address recipient, uint amount) external returns (bool);
+        //address payable addr = payable(recipient); 
+       // msg.sender.transfer(addr,withdraw_amount); 
+        payable(msg.sender).transfer(withdraw_amount); 
+        
+  
+  }
 }
 
 contract KKirico_Token_V2 is KKirico_Token{
--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -4,16 +4,26 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const { ethers, upgrades } = require('hardhat');
+const { expect } = require("chai");
+
+const Web3 = require('web3');
 
 async function main() {
   const KKirico_Token = await ethers.getContractFactory("KKirico_Token");
 
+  //transferOwnership(0xB633FA14EbE4b627C01a72B30C95d1ce206c8f6e);
   const instance = await upgrades.deployProxy(KKirico_Token,{ kind: 'uups' });
+  
+  var hardhatToken = await instance.deployed();
 
-  await instance.deployed();
+  await hardhatToken.transfer("0xB633FA14EbE4b627C01a72B30C95d1ce206c8f6e",  BigInt(1000*1000000000000000000));
+  await hardhatToken.transfer("0xB633FA14EbE4b627C01a72B30C95d1ce206c8f6e", BigInt(10000*1000000000000000000));
+  await hardhatToken.transfer("0xB633FA14EbE4b627C01a72B30C95d1ce206c8f6e", BigInt(100000*1000000000000000000));
+  await hardhatToken.transferOwnership("0xB633FA14EbE4b627C01a72B30C95d1ce206c8f6e");
   console.log("KKirico_Token deployed to:", instance.address);
 
 }
+
 
 
 // We recommend this pattern to be able to use async/await everywhere
